9ff49b3bb301461120c1b607c1f6e6e187dafc5e
Elliott Greene <elliott@kimura.uky.edu>
2023-08-22 08:10:03 -0400

    saving some changes

--- networksInvasionProbability.jl
+++ networksInvasionProbability.jl
@@ -55,11 +55,11 @@ function simulate(N = 10, T = 10, reps = 1, polyDegree = 1)
     Φ(x) = (1 - exp(-x^2))
     α = 1.0
     K = 5.0
-    polyDegree = 1
     NetSize = 5
     μ_size = .1
     fitnessHistories = [fill(0.0, T) for _ in 1:reps]
     invasionProbabilities = [fill(0.0, T) for _ in 1:reps]
+    finalNetworks = [[fill(0.0, (NetSize, NetSize)), fill(0.0, NetSize)] for _ in 1:reps]
     for r in 1:reps
         resNet = [rand(Float64, (NetSize, NetSize)), rand(Float64, NetSize)] ## Initial resident network
 
@@ -73,24 +73,43 @@ function simulate(N = 10, T = 10, reps = 1, polyDegree = 1)
             end
             fitnessHistories[r][t] = fitness(Φ, α, K, polyDegree, resNet)
         end
+
+        ## have to copy each index because of array rules
+        finalNetwork = [fill(0.0, (NetSize, NetSize)), fill(0.0, NetSize)]
+        for i in eachindex(resNet)
+            finalNetwork[i] = copy(resNet[i])
+        end
+        finalNetworks[r] = resNet
     end
-    return [fitnessHistories, invasionProbabilities]
+    return [fitnessHistories, invasionProbabilities, finalNetworks]
 end
 
 
 ## plots the fitness of each timestep in a simulation run
 ## has a comment capable of plotting the invasion probability as well
 function plotReplicatesFitness(simulationResults)
-    fitnessPlot = plot(1:T, simulationResults[1], legend = :none, title = "Fitness of all replicates")
-    invasionProbPlot = plot(1:T, simulationResults[2], legend = :none, title = "Invasion probability")
+    print(1:length(simulationResults[1]))
+    fitnessPlot = plot(1:length(simulationResults[1][1]), simulationResults[1], legend = :none, title = "Fitness of all replicates")
+    invasionProbPlot = plot(1:length(simulationResults[1][1]), simulationResults[2], legend = :none, title = "Invasion probability")
     # plot(fitnessPlot, invasionProbPlot, layout = (2,1), sharex=true) ## Returns a stacked plot of both figures
     return fitnessPlot
 end
 
 ## Samples the final network at the end of each replicate simulation
 ## plots it relative to the predicted value
-function plotResponseCurves(simulationResults, polyDegree)
+function plotResponseCurves(activation_function, activation_scale, polyDegree, simulationResults)
     ## Plotting the polynomial curve
     plt = plot(-1:0.02:1, collect([Pl(i, polyDegree) for i in -1:0.02:1]))
-    # responsePlot = plot!()
+    
+    ## Updating it with the fitness of each replicate
+    for network in simulationResults[3]
+        valueRange = -1:0.02:1 ## the range of input values the networks measure against
+        responseCurveValues = []
+        for i in valueRange
+            LayerOutputs = zeros(Float64, size(network[2])) ## size of the bias vector
+            push!(responseCurveValues, last(iterateNetwork(activation_function, activation_scale, i, network, LayerOutputs)))
+        end
+        plt = plot!(-1:0.02:1, responseCurveValues)
+    end
+    return plt
 end

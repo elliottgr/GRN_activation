dc80cc57cc758d0df8755f1d300a62829eaf3638
elliott <elliottgreene1@gmail.com>
2023-09-08 13:24:35 -0400

    saving for MCC

--- networksCompareAdaptation.jl
+++ networksCompareAdaptation.jl
@@ -1,12 +1,12 @@
 ## this file calls simulations of various network sizes to compare how this influences adaptation
 ## Network parameters explored are both the total number of nodes, as well as the distribution of these nodes (width vs depth)
-using StatsPlots ## For violin plots
+using StatsPlots, JLD2, Dates ## For violin plots
 include("networksInvasionProbability.jl") ## Could probably use the Wright-Fisher version if you wanted, but that would be much slower
 
 ## testing how the size of the network influences the final evolved fitness
 ## only varying network depth (number of layers) and keeping the number of nodes per layer the same
 ## This will be a long function since there will need to be specific code for handling the sim outputs and comparing them
-function compareNetworkSize(maxNetSize = 10, N = 10, T = 10, reps = 1, activationFunction = (f(x) = (1-exp(-x^2))), activationScale = 1.0, K = 5.0, polyDegree = 1,  μ_size = .1)
+function compareNetworkDepth(maxNetSize = 10, N = 10, T = 10, reps = 1, activationFunction = (f(x) = (1-exp(-x^2))), activationScale = 1.0, K = 5.0, polyDegree = 1,  μ_size = .1)
     netWidth = 1
     simulationResults = Array{Vector}(undef, maxNetSize)## Only saving the fitness history to save memory, should be able to retrieve networks at a later date if needed
     ## The structure of the outputs will be a three element vector of vectors
@@ -14,7 +14,6 @@ function compareNetworkSize(maxNetSize = 10, N = 10, T = 10, reps = 1, activatio
     ## showing the evolutionary history of that parameter set 
     for i in 1:maxNetSize
         simulationResults[i] = simulate(N, T, reps, activationFunction, activationScale, K, polyDegree, i, netWidth, μ_size)
-        # fitnessHistories[i] = simResults[1]
     end
     return simulationResults
 end
@@ -78,11 +77,22 @@ function fitnessHistoryViolinPlot(simulationResults)
     plt = violin(1:length(finalFitnesses), finalFitnesses[1:length(finalFitnesses)], label = labels)
 end
 
-maxNetSize = 50
-maxNetWidth = 5
-N = 1000
-T = 1000
-reps = 10
+function generateSimulations(maxNetSize = 30, N = 1000, T = 1000, reps = 10)
+    dateString = string("GRN_Adaptation_Comparisons_",Dates.now(), ".jld2")
+
+    ##Global Parameters for all simulations
+    # maxNetSize = 30
+    # maxNetWidth = 30
+    # N = 1000
+    # T = 1000
+    # reps = 10
+    networkDepthComparisons = compareNetworkDepth(maxNetSize, N, T, reps)
+    networkWidthComparisons = compareNetworkWidth(maxNetSize, maxNetSize, N, T, reps)
+    simulationResults = [networkDepthComparisons, networkWidthComparisons]
+    jldsave(dateString, simulationResults)
+end
+
+
 # simulationResults = compareNetworkSize(maxNetSize, N, T, reps)
 simulationResults = compareNetworkWidth(maxNetSize, maxNetWidth, N, T, reps)
 meanFitnessHistories = calculateMeanFitnessHistories(fitnessHistories)

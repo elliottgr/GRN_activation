ddd86f14726594106312928ee99ed0414b69515d
elliott <elliottgreene1@gmail.com>
2023-09-28 12:53:10 -0400

    adding files

---
+++ CompareAdaptations.jl
@@ -0,0 +1,86 @@
+## this file calls simulations of various network sizes to compare how this influences adaptation
+## Network parameters explored are both the total number of nodes, as well as the distribution of these nodes (width vs depth)
+using Distributed
+
+maxNetSize = 3
+maxNetWidth = 10
+N = 1000
+T = 10000
+reps = 2
+
+## Comparing different parameters for multi-processing
+nprocs()
+
+@everywhere begin
+    using Pkg; Pkg.activate(@__DIR__)
+    Pkg.instantiate(); Pkg.precompile()
+end
+
+@everywhere begin
+    using  JLD2, Dates ## For violin plots
+    include("Networks.jl")
+    function generateSimulations(maxNetSize = 30, maxNetWidth = 30, netSizeStep = 5, N = 1000, T = 1000, reps = 10)
+        dateString = string("GRN_Adaptation_Comparisons_",Dates.now(), ".jld2")
+    
+        ##Global Parameters for all simulations
+    
+        activationFunction = (f(x) = (1-exp(-x^2)))
+        activationScale = 1.0
+        K = 5.0
+        envChallenges = [1, 2, 3, 5, 7] ## Vector of each polynomial degree to check
+        # envChallenges = [3]
+        μ_size = .1
+        simulationOutputs = Dict() ## Dictionary where the keys are parameters (environmental challenge)
+        print("Beginning simulations with \n 
+                maxNetSize : $maxNetSize \n 
+                N : $N (Population size) \n 
+                T : $T (Number of timesteps) \n 
+                reps : $reps (number of replicates) \n")
+                ## nproc : $(nprocs()) (number of processes) \n"
+    
+        for polyDegree in envChallenges
+            print("Now testing Legendre Polynomials of degree $polyDegree \n")
+    
+            ## Rewritten to use multi-processing
+            
+            ## testing how the size of the network influences the final evolved fitness
+            ## only varying network depth (number of layers) and keeping the number of nodes per layer the same
+            networkDepthComparisons = []
+    
+            ## generates fitness histories for all networks of a given size
+            ## only tests networks that have the same number of total nodes, but with different depths / widths
+            networkWidthComparisons = []
+    
+            for i in 1:netSizeStep:maxNetSize
+                push!(networkDepthComparisons, simParams(N, T, reps, activationFunction, activationScale, K, polyDegree, i, 1, μ_size))
+            end
+            
+            ## Need to account for the fact that the first layer doesn't process when determining active nodes
+            ## This generates all networks of the same "active" size, meaning they have the same number of nodes outside the input layer
+            for width in 1:maxNetWidth
+                if mod(maxNetSize, width) == 0 ## only iterating with valid network sizes
+                    netDepth = Int((maxNetSize/width)+1)
+                    push!(networkWidthComparisons, simParams(N, T, reps, activationFunction, activationScale, K, polyDegree, netDepth, width, μ_size))
+                end
+            end
+            
+            outputDepthComparisons = pmap(simulate, networkDepthComparisons)
+            print("Depth comparisons of degree $polyDegree completed! \n")
+            outputWidthComparisons = pmap(simulate, networkWidthComparisons)
+            print("Width comparisons of degree $polyDegree completed! \n")
+            simulationOutputs[polyDegree] = [outputDepthComparisons, outputWidthComparisons]
+        end
+        jldsave(dateString; simulationOutputs)
+        # return simulationOutputs
+        
+    end
+    
+    maxNetSize = 12
+    maxNetWidth = 12
+    N = 1000
+    T = 1000000
+    reps = 50
+
+end 
+
+@time simulationOutputs = generateSimulations(maxNetSize, maxNetWidth, 1, N, T, reps)
--- Networks.jl
+++ Networks.jl
@@ -211,46 +211,127 @@ function mutateNetwork!(μ_size, network::Network)
     end
 end
 
-## plots the fitness of each timestep in a simulation run
-## has a comment capable of plotting the invasion probability as well
-function plotReplicatesFitness(simulationResults)
-    netDepth, netWidth = size(simulationResults[3][1])
-    numReps = length(simulationResults[3])
-    titleStr = string("Fitness of $numReps replicates for a $(netDepth) layer network with $netWidth nodes")
-
-    fitnessPlot = plot(1:length(simulationResults[1][1]), simulationResults[1], title = titleStr)
-    
-    ## This can also produce a stacked plot, uncomment below :)
-    # invasionProbPlot = plot(1:length(simulationResults[1][1]), simulationResults[2], legend = :none, title = "Invasion probability")
-    # plot(fitnessPlot, invasionProbPlot, layout = (2,1), sharex=true) ## Returns a stacked plot of both figures
-    return fitnessPlot
+## Necessary to call the distributed workers in higher level scripts, kinda pointless for single threaded versions (will still work tho)
+struct simParams
+    N::Int
+    T::Int
+    reps::Int
+    activationFunction::Function
+    activationScale::Float64
+    K::Float64
+    polyDegree::Int
+    netDepth::Int
+    netWidth::Int
+    μ_size::Float64
 end
 
+## Equivalent to Eq. 5, P(f_0 -> f_i), in le Nagard (2011)
+function invasionProbability(activationFunction, activationScale, K, polynomialDegree, N, resNet::Network, mutNet::Network)
 
-## Samples the final network at the end of each replicate simulation
-## plots it relative to the predicted value
-function plotResponseCurves(activationFunction, activationScale, polyDegree, simulationResults)
+    resFitness = fitness(activationFunction, activationScale, K, polynomialDegree, resNet)
+    mutFitness = fitness(activationFunction, activationScale, K, polynomialDegree, mutNet)
+    fitnessRatio =  resFitness / mutFitness
 
-    netDepth, netWidth = size(simulationResults[3][1])
-    titleStr = string("Network response for a $(netDepth) layer network with $netWidth nodes")
-    
-    ## Plotting the polynomial curve
-    plt = plot(-1:0.02:1, collect([PlNormalized(i, polyDegree, 0, 1) for i in -1:0.02:1]), label = "Target", title = titleStr)
-    
-    ## Updating it with the fitness of each replicate
-    for network in simulationResults[3]
-        valueRange = collect(-1:0.02:1) ## the range of input values the networks measure against
-        responseCurveValues = []
-        for i in valueRange
-            LayerOutputs = zeros(Float64, size(network)) ## size of the bias vector
-            input = fill(0.0, netWidth)
-            input[1] = i
-            push!(responseCurveValues, last(iterateNetwork!(activationFunction, activationScale, input, network, LayerOutputs)[netDepth]))
+    ## A few conditional statements broken down for debugging and dodging NaNs
+    if mutFitness == 0.0
+        fixp = 0.0
+    else
+        num = 1 - (fitnessRatio)^2
+        den = 1 - (fitnessRatio)^(2*N)
+        #JVC: the sim is designed not to have clones so need need to avoid this. 
+        #JVC: also, you may want to allow truly neutral genotypes to invade via drift.
+        if fitnessRatio == 1.0 ##ELG: Not sure if this is what the JVC comment in the above line is referring to, but this should mean that mutants with equal fitness have the expected invasion probability 
+            fixp = 1 / (2*N)
+        else
+            fixp = num/den
         end
-        plt = plot!(-1:0.02:1, responseCurveValues, alpha = 0.5)
+         ## Debugging, will flood the console but prints all fitness tests
+        #print("resFit : $resFitness  |  mutFit : $mutFitness  | fitnessRatio :  $fitnessRatio  |  Num : $num  |  Den : $den  |  Prob : $fixp \n")
     end
-    return plt
+    if isnan(fixp)
+        print("oh no")
+    end
+
+    return fixp, resFitness, mutFitness
 end
+
+## Parameters:
+
+## T = length of simulation / number of timesteps
+## N = population size
+## reps = replicates
+
+function simulate(parameters::simParams)
+
+    ## Unpacking the parameters object
+    N = parameters.N
+    T = parameters.T
+    reps = parameters.reps
+    activationFunction = parameters.activationFunction
+    activationScale = parameters.activationScale
+    K = parameters.K
+    polyDegree = parameters.polyDegree 
+    netDepth = parameters.netDepth
+    netWidth = parameters.netWidth
+    μ_size = parameters.μ_size
+
+    ## Generates a random network, then mutates it
+    netSaveStep = 1000
+    fitnessHistories = [fill(0.0, Int(T/netSaveStep)) for _ in 1:reps]
+    invasionProbabilities = [fill(0.0, Int(T/netSaveStep)) for _ in 1:reps]
+    finalNetworks = [generateFilledNetwork(netDepth, netWidth, 0.0) for _ in 1:reps]
+    for r in 1:reps
+        resNet = generateNetwork(netDepth, netWidth) ## Initial resident network
+        mutNet = copy(resNet)
+
+        ## Main timestep loop
+        for t in 1:T
+            copy!(mutNet, resNet)
+            mutateNetwork!(μ_size, mutNet)
+
+            invasionProb, resFitness, mutFitness = invasionProbability(activationFunction, activationScale, K, polyDegree, N, resNet, mutNet)
+            if mod(t, netSaveStep) == 0
+                invasionProbabilities[r][Int(t/netSaveStep)] = invasionProb
+            end
+
+            if rand() <= invasionProb
+                copy!(resNet, mutNet)
+                if mod(t, netSaveStep) == 0
+                    fitnessHistories[r][Int(t/netSaveStep)] = mutFitness
+                end
+            else
+                if mod(t, netSaveStep) == 0
+                    fitnessHistories[r][Int(t/netSaveStep)] = resFitness
+                end
+            end            
+
+        end
+
+        copy!(finalNetworks[r], resNet)
+    end
+    return [fitnessHistories, invasionProbabilities, finalNetworks]
+end
+
+## Testing the network adaptation and the response curves 
+## This section is for debugging the main simulation loops
+
+# N = 10000 ## N (population size)
+# T = 5000 ## T (simulation length)
+# reps = 2 ## number of replicates
+# activationFunction = (f(x) = (1 - exp(-x^2))) ## Le Nagard's activation function
+# # activationFunction = (f(x) = (1 / (1 + exp(-x)))) ## Logistic / sigmoid
+# # activationFunction = (f(x) = x) ## Linear activation
+# # activationFunction = (f(x) = maximum([0.0, x])) ## ReLU
+# activationScale = 1.0 ## activationScale (activation coefficient)
+# K = 5.0 ## K (strength of selection)
+# polyDegree = 3 ## degree of the Legendre Polynomial
+# netDepth = 4 ## Size of the networks
+# netWidth = 3
+# μ_size = .1 ## standard deviation of mutation magnitude
+
+# simResults = simulate(N, T, reps, activationFunction, activationScale, K, polyDegree, netDepth, netWidth, μ_size)
+# plotReplicatesFitness(simResults)
+# plotResponseCurves(activationFunction, activationScale, polyDegree, simResults)
 
 ##############################################
 ## Unit Testing and Debugging functions

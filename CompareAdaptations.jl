2909212dd84bb505116f80547cbfa5081b8c0dc4
elliott <elliottgreene1@gmail.com>
2023-10-25 12:49:26 -0400

    setting up to run on mcc

--- CompareAdaptations.jl
+++ CompareAdaptations.jl
@@ -21,7 +21,7 @@ end
         ## activation functions are Le Nagard's Exp (inverse Gaussian), the Gaussian Function, the Logistic function, and the Binary Step function
         LeNagardExp(x, α, β, γ) = 1 - exp(-x^2)
         Gaussian(x, α, β, γ) = exp(-x^2)
-        Logistic(x, α, β, γ = 1) = γ/(1+exp(-α * (x - β)))
+        Logistic(x, α = 1.0, β = 1.0, γ = 1.0) = γ/(1+exp(-α * (x - β)))
         function BinaryStep(x, α, β, γ)
             if x < 0
                 return 0.0
@@ -31,8 +31,9 @@ end
         end
         activationFunctions = [Logistic]
         activationScale = 1.0
-        α, β, γ, K = (1.0, 1.0, 1.0, 5.0)
+        α, β, γ, K = (1.0, 0.0, 1.0, 5.0)
         envChallenges = [1,2,3,4,5] ## Vector of each polynomial degree to check
+    
         μ_size = .1
         simulationOutputs = Dict() ## Dictionary where the keys are parameters (environmental challenge)
         print("Beginning simulations with \n 
@@ -44,23 +45,28 @@ end
     
         SimulationParameterSets = []
         networkSizes = []
-        for polyDegree in envChallenges
-            for activationFunction in activationFunctions
-                for i in minNetSize:netSizeStep:maxNetSize
-                    push!(SimulationParameterSets, simParams(N, T, reps, activationFunction, α, β, γ, activationScale, K, polyDegree, i, 1, μ_size))
-                    push!(networkSizes, (i, 1))
-                end
-                ## Need to account for the fact that the first layer doesn't process when determining active nodes
-                ## This generates all networks of the same "active" size, meaning they have the same number of nodes outside the input layer
-                for width in minNetWidth:maxNetWidth
-                    if mod(maxNetSize, width) == 0 ## only iterating with valid network sizes
-                        netDepth = Int((maxNetSize/width)+1)
-                        push!(SimulationParameterSets, simParams(N, T, reps, activationFunction, α, β, γ, activationScale, K, polyDegree, netDepth, width, μ_size))
-                        push!(networkSizes, (netDepth, width))
+
+        ## Main loop to create parameter sets
+        for a in [0.25, 0.5, 1.0, 2.0, 4.0, 16.0]
+            for polyDegree in envChallenges
+                for activationFunction in activationFunctions
+                    for i in minNetSize:netSizeStep:maxNetSize
+                        push!(SimulationParameterSets, simParams(N, T, reps, activationFunction, a, β, γ, activationScale, K, polyDegree, i, 1, μ_size))
+                        push!(networkSizes, (i, 1))
                     end
-                end
-            end    
+                    ## Need to account for the fact that the first layer doesn't process when determining active nodes
+                    ## This generates all networks of the same "active" size, meaning they have the same number of nodes outside the input layer
+                    for width in minNetWidth:maxNetWidth
+                        if mod(maxNetSize, width) == 0 ## only iterating with valid network sizes
+                            netDepth = Int((maxNetSize/width)+1)
+                            push!(SimulationParameterSets, simParams(N, T, reps, activationFunction, a, β, γ, activationScale, K, polyDegree, netDepth, width, μ_size))
+                            push!(networkSizes, (netDepth, width))
+                        end
+                    end
+                end    
+            end
         end
+
         print("Succesfully generated $(length(networkSizes)) parameter sets, testing network sizes (Depth, Width): \n")
         [print(x, ",  ") for x in unique(networkSizes)]
         print("\n Running simulations... \n")
@@ -74,13 +80,13 @@ end
     end
     minNetSize = 1
     minNetWidth = 1
-    maxNetSize = 4
-    maxNetWidth = 4
+    maxNetSize = 6
+    maxNetWidth = 6
     netStepSize = 1
-    N = 100
-    T = 1000
-    reps = 3
-    filestring = "PmapTesting"
+    N = 1000
+    T = 250000
+    reps = 50
+    filestring = "LogisticFunctionTests"
     
 end 
 

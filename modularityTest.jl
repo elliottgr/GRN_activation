cd14862e9b0fe5d028bba51b01090802e1bcb875
elliott <elliottgreene1@gmail.com>
2024-02-06 12:02:16 -0500

    saving progress. can now generate a few networks and test that they increase in modularity

--- modularityTest.jl
+++ modularityTest.jl
@@ -6,17 +6,32 @@ function generateParams(netDepth, netWidth, saveStep, polyDegree)
     envRange = -1:0.01:1
     LeNagardExp(x, α=sqrt(1/2), β=0.0, γ = 1.0) = 1 - (γ * exp(-((x-β)^2/(2*(α^2)))))
 
-    regulationDepth = 3
+    regulationDepth = 1
     μ_size = 0.1
     return simParams(100, 10000, saveStep, 100, LeNagardExp, sqrt(1/2), 0.0, 1.0, 1.0, 5.0, polyDegree, netDepth, netWidth, regulationDepth, μ_size, 1)
 end
 
 netDepth, netWidth = (5, 3)
-parameters = generateParams(netDepth, netWidth, 100, 3)
-network = generateFilledNetwork(netDepth, netWidth, 1.0)
+parameters = generateParams(netDepth, netWidth, 1000, 3)
+network = generateFilledNetwork(netDepth, netWidth, 0.0)
 
 
-## 
+function generateOutNets(resNet, parameters)
+    outNets = []
+    edgeSet = fill((1,1,1,1), parameters.pleiotropy)
+    for i in 1:10000
+        newNet = copy(resNet)
+        mutateNetwork!(parameters, newNet, edgeSet)
+        if fitness(parameters,newNet) >= fitness(parameters, resNet)
+            resNet = newNet
+        end
+        if mod(i, parameters.SaveStep) == 0
+            push!(outNets, copy(resNet))
+        end
+    end
+    return outNets
+end
+
 function generateGraph(network, parameters)
     netDepth, netWidth = parameters.netDepth, parameters.netWidth
     g = SimpleWeightedDiGraph()
@@ -35,15 +50,18 @@ function generateGraph(network, parameters)
     partitions = fill(1, netWidth) ## creating
 
     ## generating all edges
+    edges = 1
     for i in 2:netDepth
+        edges += 1
         for j in 1:netWidth
-            edges = 0
+
             for k in maximum([1, i-parameters.regulationDepth]):i-1
                 for l in 1:netWidth
                     add_edge!(g, NodeDict[k,l], NodeDict[i,j], network.Wm[i, j][k, l])
-                    edges += 1
+
                 end
             end
+
             push!(partitions, edges)
         end
     end
@@ -51,5 +69,8 @@ function generateGraph(network, parameters)
     return g, partitions
 end
 
-g, partitions = generateGraph(network, parameters)
-modularity(g, partitions)
+outNets = generateOutNets(network, parameters)
+for net in outNets
+    g, partitions = generateGraph(net, parameters)
+    print("\n",modularity(g, partitions), "\n")
+end
